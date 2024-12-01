477bc65808075551a389514e271928eff12c01c0
максим <maximovichpro@gmail.com>
2024-12-01 22:21:47 +0500

    Implement statistic to index page

--- apps/core/views.py
+++ apps/core/views.py
@@ -7,12 +7,58 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.static import serve
 from django.contrib.auth.decorators import login_required
 from django.utils.translation import gettext_lazy as _
+from django.db.models import Sum, Count
+from django.db.models.functions import TruncMonth
 
+from apps.invoices.models import Invoice
+from apps.products.models import Supplier
 
 class IndexView(LoginRequiredMixin, TemplateView):
     """Class-based view for index page."""
 
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        # invoices
+        monthly_invoices = (
+            Invoice.objects
+            .annotate(month=TruncMonth("deadline"))
+            .values("month")
+            .annotate(total_amount=Sum("amount_to_pay"))
+            .order_by("month")
+        )
+        invoice_statistic = []
+        for entry in monthly_invoices:
+            invoice_statistic.append({
+                "month": _(entry["month"].strftime("%B")),
+                "total_amount": float(entry["total_amount"]) or 0
+            })
+
+        total_invoices_sum = Invoice.objects.aggregate(
+            total=Sum("amount_to_pay"),
+        )["total"]
+
+        # suppliers
+        supplier_invoices_count = (
+            Supplier.objects
+            .annotate(invoice_count=Count("invoices"))
+            .values("id", "name", "invoice_count")
+        )
+
+        suppliers_statistic = []
+        for entry in supplier_invoices_count:
+            suppliers_statistic.append({
+                "name": entry["name"],
+                "invoice_count": entry["invoice_count"] or 0
+            })
+        total_suppliers = len(Supplier.objects.all())
+
+        context = super().get_context_data(**kwargs)
+        context["invoice_statistic"] = invoice_statistic
+        context["suppliers_statistic"] = suppliers_statistic
+        context["total_invoices_sum"] = total_invoices_sum
+        context["total_suppliers"] = total_suppliers
+        return context
 
 
 @login_required
